291eb9323b18e159d2eea28f523e6267e813b418
Safo Prince <psam64850@gmail.com>
2022-05-30 19:17:50 +0000

    added the sidebar button

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -9,12 +9,13 @@ import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <TwitterIcon />
-      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notification" />
       <SidebarOption Icon={MailOutlineIcon} text="Messages" />
@@ -22,6 +23,11 @@ const Sidebar = () => {
       <SidebarOption Icon={ListAltIcon} text="Lists" />
       <SidebarOption Icon={PermIdentityIcon} text="Profile" />
       <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        {" "}
+        Tweet{" "}
+      </Button>
     </div>
   );
 };
