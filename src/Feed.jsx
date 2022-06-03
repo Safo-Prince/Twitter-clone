1044582367754faf925ed9d4d6d21c364b85db86
Safo Prince <psam64850@gmail.com>
2022-06-03 22:51:50 +0000

    completed the sidebar component

---
+++ src/Feed.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div className="header">
+      <div className="feed__header">
+        <h2> home</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- src/SidebarOption.jsx
+++ src/SidebarOption.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./SidebarOption.css";
 
 const SidebarOption = ({ active, text, Icon }) => {
   return (
     <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
       <Icon />
+
       <h2> {text} </h2>
     </div>
   );
